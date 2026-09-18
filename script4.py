34acf806a5b11fa0457fd521ce2970bc4fc9818e
vaibhavbatham13-code <vaibhavbatham13@gmail.com>
2026-09-18 17:41:55 +0530

    Refactor weather and travel planning agents

--- script4.py
+++ script4.py
@@ -6,7 +6,7 @@ import requests
 import streamlit as st
 llm = ChatGoogleGenerativeAI(
   model="gemini-2.5-flash",temperature=0,
-                 "GOOGLE_API_KEY"=st.secrets["GOOGLE_API_KEY"])
+                 google_api_key=st.secrets["GOOGLE_API_KEY"])
 class TravelState(TypedDict):
         destination: str
         days: int
