c53b0dcd7d88ce5dfabad45f6b69bb3c081d5148
vaibhavbatham13-code <vaibhavbatham13@gmail.com>
2026-09-18 18:01:13 +0530

    Update script4.py

--- script4.py
+++ script4.py
@@ -5,7 +5,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import requests
 import streamlit as st
 llm = ChatGoogleGenerativeAI(
-  model="gemini-2.5-flash",temperature=0,
+  model="gemini-3.6-flash",temperature=0,
                  google_api_key=st.secrets["GOOGLE_API_KEY"])
 class TravelState(TypedDict):
         destination: str
